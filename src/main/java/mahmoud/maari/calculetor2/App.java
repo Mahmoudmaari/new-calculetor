f733e734e699af201d0f8846a7f56726021dcb27
Mahmoudmaari <Mahmoudmaari@users.noreply.github.com>
2018-12-13 16:23:01 +0100

    new calculator

---
+++ src/main/java/mahmoud/maari/calculetor2/App.java
@@ -0,0 +1,93 @@
+package mahmoud.maari.calculetor2;
+
+import java.util.Scanner;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+	private static Scanner in = new Scanner (System.in);
+	private static Scanner um = new Scanner (System.in);
+	
+	
+    public static void main( String[] args )
+    
+    {
+    	
+    	
+    	
+    	
+    		
+    	
+    	double result = 0;
+    	
+        System.out.println("put operator");
+    	String operator = in.nextLine();
+    	
+    	System.out.println("first number");
+    	double num1 = in.nextDouble();
+    	
+    	
+    	
+    	System.out.println("second number");
+    	double num2 = in.nextDouble();
+    	
+    	
+    	switch(operator) {
+    	case"+":result= num1+ num2;
+    	break;
+    	case"-":result= num1-num2;
+    	break;
+    	case"*":result= num1* num2;
+    	break;
+    	case"/":
+    		if (num1 == 0 || num2 ==0) {
+        		result =0 ;
+        	}else {
+        		result= num1/ num2;
+        	}
+    		
+    		break;
+    	default:System.out.println(operator+ " is not supported");
+    	
+    	
+    	
+    	
+    	}
+    	
+    		
+    	System.out.println(result); 
+    	
+    	
+    	
+  	    
+    	System.out.println("press any think");
+    	
+    	String com1 = um.nextLine();
+    	
+    	switch (com1) {
+    	case "n":com = false;
+    	default :
+    	
+    	
+    		System.out.println("good bye");
+    	}
+    
+    	
+    	
+  	    }
+    
+    	
+    	}
+
+
+	
+	
+    	
+    		
+		
+    	
