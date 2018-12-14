2cb7082d7d58d9a266c7ded7f363c1bc9d39f3f8
Mahmoudmaari <Mahmoudmaari@users.noreply.github.com>
2018-12-14 15:42:47 +0100

    version 2.0.1

--- src/main/java/mahmoud/maari/calculetor2/App.java
+++ src/main/java/mahmoud/maari/calculetor2/App.java
@@ -2,70 +2,95 @@ package mahmoud.maari.calculetor2;
 
 import java.util.Scanner;
 
-
 /**
  * Hello world!
  *
  */
 public class App 
-{
-
-	private static Scanner in = new Scanner (System.in);
-	private static Scanner um = new Scanner (System.in);
-	
-	
+{ private static Scanner in = new Scanner (System.in);
     public static void main( String[] args )
-    
     {
-    	boolean com = true;
-    	double result = 0;
+    	boolean run = true, stop = false;
     	
-        System.out.println("put operator");
-    	String operator = in.nextLine();
-    	
-    	System.out.println("first number");
-    	double num1 = in.nextDouble();
-    	
-    	System.out.println("second number");
-    	double num2 = in.nextDouble();
-    	
-    	
-    	switch(operator) {
-    	case"+":result= num1+ num2;
-    	break;
-    	case"-":result= num1-num2;
-    	break;
-    	case"*":result= num1* num2;
-    	break;
-    	case"/":
-    		if (num1 == 0 || num2 ==0) {
-        		result =0 ;
-        	}else {
-        		result= num1/ num2;
-        	}
+    	while (run) {
+    		double num1, num2, result;
     		
+    		result = 0;
+    		System.out.println("put your opretor");
+    		String op = getInput();	
+    		
+    		System.out.println("first number");
+    		num1 = getValidNumber();
+    		
+    		System.out.println("second number");
+    		num2 = getValidNumber();
+    		
+    		
+    		switch (op) {
+    		
+    		case "+" : result = num1 + num2; 
     		break;
-    	default:System.out.println("      " + "("+operator+")" + "  Error put right operator");}
-    	System.out.println(result); 
-    	
-
-    	System.out.println("press any key");
-    	String com1 = um.nextLine();
-    	
-    	switch (com1) {
-    	case "n":com = false;
-    	default :
-    	
-    	
-        System.out.println("good bye");
-    	}
-  	}
-}
-
-
-	
-	
-    	
+    		case "-" : result = num1 - num2; 
+    		break;
+    		case "*" : result = num1 * num2; 
+    		break;
+    		case "/" : if (num1 == 0 || num2 == 0) {
+    			result = 0;
+    		}else {
+    			result = num1 / num2;
+    		}
+    		break;
     		
-		
+    		
+    		}
+    		
+    		
+    		System.out.println ( num1 + op + num2 + "=" + result);
     	
+    	
+    	System.out.println("press (y) to continue or press any key to end");
+		String op1 = getInput();	
+    		switch (op1) {
+    		case "Y"  :
+    		case "y" : run = true;
+    		break;
+    		default : run = false;
+    		System.out.println("good bye");
+    		}
+    	
+    	}
+    	
+    
+    
+    }
+   
+ 
+   
+
+   public static String getInput() {
+   	return in.nextLine();
+   }
+   
+   public static double getValidNumber() {
+   	boolean valid = false;
+   	double number = 0;
+   	while(!valid) {
+   		try {
+       		number = Double.parseDouble(getInput());
+       		valid = true;
+       	}catch(NumberFormatException e) {
+       		System.out.println("You did not enter a valid number");
+       	}
+   	}
+   	return number;    	
+   }
+ 
+    
+    
+  
+    
+    
+    
+  	  
+    
+}
