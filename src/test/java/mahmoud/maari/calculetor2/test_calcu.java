8d75059ff616065c9291277542b7cc586402711f
Mahmoudmaari <Mahmoudmaari@users.noreply.github.com>
2019-03-25 13:46:06 +0100

    ver 2.0.2

--- src/test/java/mahmoud/maari/calculetor2/test_calcu.java
+++ src/test/java/mahmoud/maari/calculetor2/test_calcu.java
@@ -10,36 +10,36 @@ public class test_calcu {
 	@Test
 	public void test_cal() {
 		double n1=10,n2=5,r=0;
-		Calcouletor cal = new Calcouletor(n1, "+", n2, r);
+		Calcouletor cal = new Calcouletor(n1, "+", n2);
 		double expected=15;
-		double actual =cal.add(n1, n2, r);
+		double actual =cal.add(n1, n2);
 		assertEquals(expected, actual,0);
 		
 	}
 	@Test
 	public void test_cal1() {
 		double n1=10,n2=5,r=0;
-		Calcouletor cal = new Calcouletor(n1, "-", n2, r);
+		Calcouletor cal = new Calcouletor(n1, "-", n2);
 		double expected=5;
-		double actual =cal.sub(n1, n2, r);
+		double actual =cal.sub(n1, n2);
 		assertEquals(expected, actual,0);
 		
 	}
 	@Test
 	public void test_cal2() {
 		double n1=10,n2=5,r=0;
-		Calcouletor cal = new Calcouletor(n1, "*", n2, r);
+		Calcouletor cal = new Calcouletor(n1, "*", n2);
 		double expected=50;
-		double actual =cal.malt(n1, n2, r);
+		double actual =cal.malt(n1, n2);
 		assertEquals(expected, actual,0);
 		
 	}
 	@Test
 	public void test_cal3() {
 		double n1=10,n2=2,r=0;
-		Calcouletor cal = new Calcouletor(n1, "*", n2, r);
+		Calcouletor cal = new Calcouletor(n1, "*", n2);
 		double expected=5;
-		double actual =cal.deva(n1, n2, r);
+		double actual =cal.deva(n1, n2);
 		assertEquals(expected, actual,0);
 		
 	}
